e5dacac6d8c3a09d9000c4c45de2082fb4d4bf9e
gbh10c <grandcasa10@gmail.com>
2022-05-23 22:44:19 -0400

    useState examples

--- advanced-react-projects/src/tutorial/1-useState/setup/2-useState-basics.js
+++ advanced-react-projects/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
 
+// hooks start with 'use'
+// the component name must be uppercase (see 'UseStateBasics' below)
+// the hook must be in the function/component body
+// cannot call the hook conditionally
+
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('Random Title');
+
+  const handleClick = () => {
+    if (text === 'Random Title') {
+      setText('Hello, World!');
+    } else {
+      setText('Random Title');
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
