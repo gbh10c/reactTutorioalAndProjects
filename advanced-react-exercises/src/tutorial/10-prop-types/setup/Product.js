420721738885aaefb211ab11847e8dfa65f5a3f8
gbh10c <grandcasa10@gmail.com>
2022-05-30 11:12:28 -0400

    Completed PropTypes exercise and included file with shortcut instructions

--- advanced-react-exercises/src/tutorial/10-prop-types/setup/Product.js
+++ advanced-react-exercises/src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, name, price }) => {
+  // CHECK IF IMAGE EXISTS SO THAT THE SHORT CIRCUIT WILL WORK INSIDE THE ARTICLE
+  const url = image && image.url;
+  return (
+    <article className='product'>
+      <img src={url || defaultImage} alt={name || 'default name'} />
+      <h4>{name || 'default name'}</h4>
+      <p>${price || 3.99}</p>
+    </article>
+  );
 };
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+// ONE OPTION FOR DEFAULT PROPS
+// Product.defaultProps = {
+//   name: 'default name',
+//   price: 3.99,
+//   image: defaultImage,
+// };
 
 export default Product;
---
+++ advanced-react-exercises/src/tutorial/10-prop-types/setup/shortcutSetup.js
@@ -0,0 +1,23 @@
+// rafcp will produce this:
+
+import React from 'react'
+import PropTypes from 'prop-t
+
+ypes'
+
+const testing = props => {
+  return (
+    <div>
+      testing
+    </div>
+
+
+    )
+}
+
+testing.propTypes = {
+  // name:ptar preceded by a square produces
+  name:PropTypes.array.isRequired
+}
+
+export default testing
