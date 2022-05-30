1a97c1c44b61e631d2cf6362e83ca3202a4b0fc1
gbh10c <grandcasa10@gmail.com>
2022-05-29 21:36:53 -0400

    Completed Stripe project

--- react-projects-master/13-stripe-submenus/setup/src/Navbar.js
+++ react-projects-master/13-stripe-submenus/setup/src/Navbar.js
@@ -5,28 +5,45 @@ import { useGlobalContext } from './context';
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
-
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent;
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    openSubmenu(page, { center, bottom });
+  };
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains('link-btn')) {
+      closeSubmenu();
+    }
+  };
   return (
-    <nav className='nav'>
+    <nav className='nav' onMouseOver={handleSubmenu}>
       <div className='nav-center'>
-        <div className='nav-hea'>
-          <img src={logo} className='nav-logo' alt='stripe' />
+        <div className='nav-header'>
+          <img src={logo} className='nav-logo' alt='' />
           <button className='btn toggle-btn' onClick={openSidebar}>
             <FaBars />
           </button>
         </div>
         <ul className='nav-links'>
           <li>
-            <button className='link-btn'>products</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              products
+            </button>
           </li>
           <li>
-            <button className='link-btn'>developers</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              developers
+            </button>
           </li>
           <li>
-            <button className='link-btn'>company</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              company
+            </button>
           </li>
         </ul>
-        <button className='btn singin-btn'>sign in</button>
+        <button className='btn signin-btn'>sign in</button>
       </div>
     </nav>
   );
